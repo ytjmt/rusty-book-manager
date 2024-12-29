36594a20bb33fe1aff81914d15174653bf98296b
ytjmt <46666464+ytjmt@users.noreply.github.com>
2024-12-29 17:27:14 +0900

    Implement health check endpoint and set up Axum server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::{Ipv4Addr, SocketAddr};
+
+use anyhow::Result;
+use axum::{http::StatusCode, routing::get, Router};
+use tokio::net::TcpListener;
+
+pub async fn health_check() -> StatusCode {
+    StatusCode::OK
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let app = Router::new().route("/health", get(health_check));
+    let addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 8080);
+    let listener = TcpListener::bind(addr).await?;
+    println!("Listening on {}", addr);
+    Ok(axum::serve(listener, app).await?)
 }
