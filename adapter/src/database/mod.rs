1027e1e4ae109df542bd378b169146a572011eed
ytjmt <46666464+ytjmt@users.noreply.github.com>
2025-01-04 23:20:43 +0900

    Implement book repository

--- adapter/src/database/mod.rs
+++ adapter/src/database/mod.rs
@@ -1,6 +1,8 @@
 use shared::config::DatabaseConfig;
 use sqlx::{postgres::PgConnectOptions, PgPool};
 
+pub mod model;
+
 // DatabaseConfigからPgConnectOptionsに変換
 fn make_pg_connect_options(cfg: &DatabaseConfig) -> PgConnectOptions {
     PgConnectOptions::new()
@@ -15,6 +17,9 @@ fn make_pg_connect_options(cfg: &DatabaseConfig) -> PgConnectOptions {
 #[derive(Clone)]
 pub struct ConnectionPool(PgPool);
 impl ConnectionPool {
+    pub fn new(pool: PgPool) -> Self {
+        Self(pool)
+    }
     pub fn inner_ref(&self) -> &PgPool {
         &self.0
     }
---
+++ adapter/src/repository/book.rs
@@ -0,0 +1,122 @@
+use anyhow::Result;
+use async_trait::async_trait;
+use derive_new::new;
+use kernel::{
+    model::book::{event::CreateBook, Book},
+    repository::book::BookRepository,
+};
+use uuid::Uuid;
+
+use crate::database::model::book::BookRow;
+use crate::database::ConnectionPool;
+
+#[derive(new)]
+pub struct BookRepositoryImpl {
+    db: ConnectionPool,
+}
+
+#[async_trait]
+impl BookRepository for BookRepositoryImpl {
+    async fn create(&self, event: CreateBook) -> Result<()> {
+        sqlx::query!(
+            r#"
+                INSERT INTO books (title, author, isbn, description)
+                VALUES($1, $2, $3, $4)
+            "#,
+            event.title,
+            event.author,
+            event.isbn,
+            event.description
+        )
+        .execute(self.db.inner_ref())
+        .await?;
+
+        Ok(())
+    }
+
+    async fn find_all(&self) -> Result<Vec<Book>> {
+        let rows: Vec<BookRow> = sqlx::query_as!(
+            BookRow,
+            r#"
+                SELECT
+                    book_id,
+                    title,
+                    author,
+                    isbn,
+                    description
+                FROM books
+                ORDER BY created_at DESC
+            "#
+        )
+        .fetch_all(self.db.inner_ref())
+        .await?;
+
+        Ok(rows.into_iter().map(Book::from).collect())
+    }
+
+    async fn find_by_id(&self, book_id: Uuid) -> Result<Option<Book>> {
+        let row: Option<BookRow> = sqlx::query_as!(
+            BookRow,
+            r#"
+                SELECT
+                    book_id,
+                    title,
+                    author,
+                    isbn,
+                    description
+                FROM books
+                WHERE book_id = $1
+            "#,
+            book_id
+        )
+        .fetch_optional(self.db.inner_ref())
+        .await?;
+
+        Ok(row.map(Book::from))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[sqlx::test]
+    async fn test_register_book(pool: sqlx::PgPool) -> anyhow::Result<()> {
+        let repo = BookRepositoryImpl::new(ConnectionPool::new(pool));
+
+        let book = CreateBook {
+            title: "Test Title".into(),
+            author: "Test Author".into(),
+            isbn: "Test ISBN".into(),
+            description: "Test Description".into(),
+        };
+
+        // 蔵書データを投入すると正常終了することを確認
+        repo.create(book).await?;
+
+        // 蔵書の一覧を取得すると投入した1件だけ取得できることを確認
+        let res = repo.find_all().await?;
+        assert_eq!(res.len(), 1);
+
+        // 蔵書の一覧の最初のデータから蔵書IDを取得し、find_by_idメソッドでその蔵書データを取得できることを確認
+        let book_id = res[0].id;
+        let res = repo.find_by_id(book_id).await?;
+        assert!(res.is_some());
+
+        // 取得した蔵書データがCreateBookで投入した蔵書データと一致することを確認
+        let Book {
+            id,
+            title,
+            author,
+            isbn,
+            description,
+        } = res.unwrap();
+        assert_eq!(id, book_id);
+        assert_eq!(title, "Test Title");
+        assert_eq!(author, "Test Author");
+        assert_eq!(isbn, "Test ISBN");
+        assert_eq!(description, "Test Description");
+
+        Ok(())
+    }
+}
