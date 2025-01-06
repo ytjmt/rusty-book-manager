9d37e877f03f6d14fc259c017b88de58eee597ed
ytjmt <46666464+ytjmt@users.noreply.github.com>
2025-01-06 23:15:37 +0900

    Add users and roles tables

--- adapter/migrations/20250104104802_start.down.sql
+++ adapter/migrations/20250104104802_start.down.sql
@@ -2,4 +2,10 @@ DROP TRIGGER IF EXISTS books_updated_at_trigger ON books;
 
 DROP TABLE IF EXISTS books;
 
+DROP TRIGGER IF EXISTS users_updated_at_trigger ON books;
+
+DROP TABLE IF EXISTS users;
+
+DROP TABLE IF EXISTS roles;
+
 DROP FUNCTION set_updated_at;
