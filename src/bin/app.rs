b71ccec8eed81ef285a62445e10bd65692cf85fa
ytjmt <46666464+ytjmt@users.noreply.github.com>
2025-10-08 00:52:45 +0900

    Fix for test

--- src/bin/app.rs
+++ src/bin/app.rs
@@ -7,7 +7,7 @@ use adapter::{database::connect_database_with, redis::RedisClient};
 use anyhow::{Context, Result};
 use api::route::{auth, v1};
 use axum::{http::Method, Router};
-use registry::AppRegistry;
+use registry::AppRegistryImpl;
 use shared::{
     config::AppConfig,
     env::{which, Environment},
@@ -31,7 +31,7 @@ async fn bootstrap() -> Result<()> {
     let app_config = AppConfig::new()?;
     let pool = connect_database_with(&app_config.database);
     let kv = Arc::new(RedisClient::new(&app_config.redis)?);
-    let registry = AppRegistry::new(pool, kv, app_config);
+    let registry = Arc::new(AppRegistryImpl::new(pool, kv, app_config));
 
     let app = Router::new()
         .merge(v1::routes())
